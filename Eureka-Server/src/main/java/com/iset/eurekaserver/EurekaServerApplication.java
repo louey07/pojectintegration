1561daab7d4a9a1da4a897774f98909942199a5b
louey07 <loueysouilhi08@gmail.com>
2025-12-23 15:31:27 +0100

    Ajout Eureka-Server

---
+++ Eureka-Server/src/main/java/com/iset/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.iset.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer  // ← Enable Eureka Server
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
